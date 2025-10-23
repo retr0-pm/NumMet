e9980fb233e351b542833d759bc8f9cd18b4ba34
retr0 <nikitaerx@gmail.com>
2025-10-24 01:41:14 +0300

    Base

---
+++ O2/pages/8_Заключение.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+r"""
+#### Вывод по общему назначению  Matplotlib
+"""
---
+++ O2/Введение.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+r"""
+#### Библиотека для визуализации данных Matplotlib
+"""
